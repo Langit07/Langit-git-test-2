c061b56be65e7baa978c03c8b0e61fe1b6cd19cb
Langit07 <caturpelit123@gmail.com>
2022-11-04 08:43:36 +0700

    menambahkan file index.php

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>coba git remote</title>
 </head>
 <body>
     <h1>Hello world!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In libero, minus non eligendi ex placeat. Quam suscipit necessitatibus natus odio dolor ducimus dolores fugit qui enim autem velit, similique nostrum!</p>
 </body>
 </html>

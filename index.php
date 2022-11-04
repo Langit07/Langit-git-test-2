47e889225eca82e2657720540926b7fad54b18b8
Langit07 <caturpelit123@gmail.com>
2022-11-04 08:46:12 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam voluptate, unde amet praesentium quod eum ad quaerat ipsum obcaecati adipisci vitae repellendus minus sapiente. Perspiciatis obcaecati asperiores ut error deserunt!</p>
 </body>
 </html>
